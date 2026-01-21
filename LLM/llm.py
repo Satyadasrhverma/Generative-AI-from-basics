7812e5f7be976555f0bfd30e2424a743f85f5e01
Satyadarsh verma <satya12darsh@gmail.com>
2026-01-21 13:44:49 +0530

    initial commit

---
+++ LLM/llm.py
@@ -0,0 +1,14 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation"
+) 
+
+model = ChatHuggingFace(llm = llm)
+
+
+result = model.invoke("Who is narendra Modi")
+print(result)

970f3f6fa63d06e495b5f47a2668b3656690156d
Satyadarsh verma <satya12darsh@gmail.com>
2026-01-22 13:33:58 +0530

    prompt

---
+++ Structure output parser/str_output_paser.py
@@ -0,0 +1,33 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import 
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation"
+) 
+
+model = ChatHuggingFace(llm = llm)
+
+
+template1 = PromptTemplate(
+    template= 'write a detailed report on {topic}',
+    input_variables= ['topic']
+    
+)
+
+template2 = PromptTemplate(
+    template= "write a 5 line summary on the following text./n {text}",
+    input_variables= ['text']
+)
+
+prompt = template1.invoke({'topic' : "Phsyics"})
+
+result = model.invoke(prompt)
+
+prompt2 = template2.invoke({'text' : result.content})
+
+out = model.invoke(prompt2)
+print(out.content)

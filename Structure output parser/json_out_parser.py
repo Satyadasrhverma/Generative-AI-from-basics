acd064674ee4cc6e86851ae78b403afdc8d1c2d2
Satyadarsh verma <satya12darsh@gmail.com>
2026-01-22 14:04:19 +0530

    jsonoutput parser

---
+++ Structure output parser/json_out_parser.py
@@ -0,0 +1,32 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation"
+) 
+
+model = ChatHuggingFace(llm = llm)
+
+parser = JsonOutputParser()
+
+template = PromptTemplate(
+    template= 'give me the name,age and city of a fictional person\n {format_instruction}',
+    partial_variables= {'format_instruction': parser.get_format_instructions()}
+    
+)
+
+#prompt = template.format()
+#result = model.invoke(prompt)
+
+#out =parser.parse(result.content)
+#print(out)
+
+chain = template | model | parser
+
+result = chain.invoke({})
+
+print(result)
